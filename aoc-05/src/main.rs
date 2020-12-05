c489140d80df8ef544c1e523af96e56edcd4de09
Karl Barrus <klbarrus@yahoo.com>
2020-12-05 01:59:50 -0500

    Day 05, part 2

--- aoc-05/src/main.rs
+++ aoc-05/src/main.rs
@@ -17,7 +17,7 @@ fn parse_pass(s: &str) -> (u16, u16, u16) {
 
     let r = row.iter().fold(0, |acc, x| acc * 2 + x);
     let c = col.iter().fold(0, |acc, x| acc * 2 + x);
-    let id = r*8 + c;
+    let id = r * 8 + c;
 
     (r, c, id)
 }
@@ -26,15 +26,27 @@ fn main() -> io::Result<()> {
     let lines = aoclib::read_file_lines(&"aoc-05.txt")?;
 
     let passes: Vec<(u16, u16, u16)> = lines.iter().map(|s| parse_pass(s)).collect();
-//    println!("{:#?}", passes);
 
-    let minmax = passes.iter().minmax_by_key(|(r,c,id)| id);
-    let maxid = match minmax {
-        MinMaxResult::MinMax(a, b) => b.2,
-        _ => 0,
+    let minmax = passes.iter().minmax_by_key(|(_r, _c, id)| id);
+    let (minid, maxid) = match minmax {
+        MinMaxResult::MinMax(a, b) => (a.2, b.2),
+        _ => (0, 0),
     };
 
     println!("Part 1: {}", maxid);
+
+    let mut seatids = passes.iter().map(|(_r, _c, id)| id).collect::<Vec<&u16>>();
+    seatids.sort();
+
+    // search from the first seat
+    let mut myseat = minid;
+    loop {
+        myseat = myseat + 1;
+        if !seatids.contains(&&myseat) {
+            println!("Part 2: {}", myseat);
+            break;
+        }
+    }
 
     Ok(())
 }
