9463116faf8d6a25b05f103fcc46e885761b4968
Karl Barrus <klbarrus@yahoo.com>
2021-02-01 17:38:45 -0500

    Day 07, iterator parse into HashMap

--- aoc-07/src/main.rs
+++ aoc-07/src/main.rs
@@ -2,7 +2,8 @@
 
 use aoclib;
 use itertools::Itertools;
-use std::collections::HashSet;
+use std::collections::HashMap;
+use std::vec::Vec;
 use std::io;
 
 fn main() -> io::Result<()> {
@@ -17,10 +18,19 @@ fn main() -> io::Result<()> {
                 a.strip_suffix(" bags").unwrap(),
                 b.split(", ")
                     .flat_map(|x| x.rsplitn(2, " ").skip(1).collect::<Vec<_>>())
+                    //.iter()
+                    .map(|a| {
+                        let v = a.splitn(2, " ").collect::<Vec<_>>();
+                        if v[0] == "no" {
+                            (0, "")
+                        } else {
+                            (v[0].parse::<i32>().unwrap(), v[1])
+                        }
+                    })
                     .collect::<Vec<_>>(),
             )
         })
-        .collect::<Vec<(_, _)>>();
+        .collect::<HashMap<_, _>>();
 
     println!("{:#?}", part1_data);
 
