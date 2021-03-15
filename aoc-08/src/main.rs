69d312aade6a9fd2625dfa7c4add1ee8b2788d3e
Karl Barrus <klbarrus@yahoo.com>
2021-03-15 17:46:42 -0400

    Day 08, refactor part 1

--- aoc-08/src/main.rs
+++ aoc-08/src/main.rs
@@ -5,23 +5,14 @@ use itertools::Itertools;
 use std::collections::HashSet;
 use std::io;
 
-fn main() -> io::Result<()> {
-    let lines = aoclib::read_file_lines(&"aoc-08.txt")?;
-
-    let prog_data = lines
-        .iter()
-        .flat_map(|l| l.split(" "))
-        .tuples()
-        .map(|(a, b)| (a, b.parse::<i64>().unwrap()))
-        .collect::<Vec<(_, _)>>();
-
-    let mut curr_inst: usize = 0;
-    let mut acc_val: i64 = 0;
+fn run_prog(prog_data: &Vec<(&str, i64)>, start_inst: usize) -> (bool, i64) {
     let mut done: bool = false;
-    let mut already_run = HashSet::<usize>::new();
-    already_run.insert(0); // first line of program is always run
+    let mut looping: bool = true;
+    let mut curr_inst: usize = start_inst;
+    let mut acc_val: i64 = 0;
 
-    //println!("{:#?}", prog_data);
+    let mut already_run = HashSet::<usize>::new();
+    already_run.insert(curr_inst);
 
     while !done {
         let curr_op: &str = prog_data[curr_inst].0;
@@ -47,12 +38,30 @@ fn main() -> io::Result<()> {
             }
         }
 
-        // println!("inst: {}, acc_val: {}", curr_inst, acc_val);
-
-        // HashSet::insert() returns true if set did not have the value
         done = !already_run.insert(curr_inst);
+
+        if curr_inst > prog_data.len() - 1 {
+            done = true;
+            looping = false;
+        }
     }
 
+    (looping, acc_val)
+}
+
+fn main() -> io::Result<()> {
+    let lines = aoclib::read_file_lines(&"aoc-08.txt")?;
+
+    let prog_data = lines
+        .iter()
+        .flat_map(|l| l.split(" "))
+        .tuples()
+        .map(|(a, b)| (a, b.parse::<i64>().unwrap()))
+        .collect::<Vec<(_, _)>>();
+
+    //println!("{:#?}", prog_data);
+
+    let (_, acc_val) = run_prog(&prog_data, 0);
     println!("Part 1: {}", acc_val);
 
     Ok(())
