f7469cfa784f5424139c4cb7ffec6a170048f2be
Karl Barrus <klbarrus@yahoo.com>
2021-04-01 14:20:03 -0400

    Day 08 - part 2

--- aoc-08/src/main.rs
+++ aoc-08/src/main.rs
@@ -5,9 +5,13 @@ use itertools::Itertools;
 use std::collections::HashSet;
 use std::io;
 
-fn run_prog(prog_data: &Vec<(&str, i64)>, start_inst: usize) -> (bool, i64) {
+fn run_prog(
+    prog_data: &Vec<(&str, i64)>,
+    start_inst: usize,
+    error_inst: Option<usize>,
+) -> (bool, i64) {
     let mut done: bool = false;
-    let mut looping: bool = true;
+    let mut looping: bool = true;   // simulation exited due to loop detection
     let mut curr_inst: usize = start_inst;
     let mut acc_val: i64 = 0;
 
@@ -15,8 +19,17 @@ fn run_prog(prog_data: &Vec<(&str, i64)>, start_inst: usize) -> (bool, i64) {
     already_run.insert(curr_inst);
 
     while !done {
-        let curr_op: &str = prog_data[curr_inst].0;
+        let mut curr_op: &str = prog_data[curr_inst].0;
         let curr_val = prog_data[curr_inst].1;
+
+        // if the current instruction is the simulate error, flip the instruction
+        if Some(curr_inst) == error_inst {
+            if curr_op == "jmp" {
+                curr_op = "nop";
+            } else {
+                curr_op = "jmp";
+            }
+        }
 
         match curr_op {
             "acc" => {
@@ -41,6 +54,7 @@ fn run_prog(prog_data: &Vec<(&str, i64)>, start_inst: usize) -> (bool, i64) {
         done = !already_run.insert(curr_inst);
 
         if curr_inst > prog_data.len() - 1 {
+            // simulation exited at the end of the program, set looping false
             done = true;
             looping = false;
         }
@@ -59,10 +73,22 @@ fn main() -> io::Result<()> {
         .map(|(a, b)| (a, b.parse::<i64>().unwrap()))
         .collect::<Vec<(_, _)>>();
 
-    //println!("{:#?}", prog_data);
+    // println!("{:#?}", prog_data);
 
-    let (_, acc_val) = run_prog(&prog_data, 0);
+    let (_, acc_val) = run_prog(&prog_data, 0, None);
     println!("Part 1: {}", acc_val);
+
+    for i in 0..prog_data.len() {
+        if prog_data[i].0 == "nop" || prog_data[i].0 == "jmp" {
+            let (looping, acc_val) = run_prog(&prog_data, 0, Some(i));
+
+            if !looping {
+                println!("Part 2: {}", acc_val);
+                println!("  error was instruction {}: {}", i, prog_data[i].0);
+                break;
+            }
+        }
+    }
 
     Ok(())
 }
